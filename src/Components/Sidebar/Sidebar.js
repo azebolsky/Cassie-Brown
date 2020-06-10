89ef1939b709763888a93f5a406097184416309c
ADAM <zebolsky.adam@gmail.com>
2020-06-10 14:34:31 -0500

    first commit

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const sidebar = () => {
+  return (
+    <div className="Sidebar">
+      <ul>
+        <li>
+          <button>Home</button>
+        </li>
+        <li>
+          <button>About</button>
+        </li>
+        <li>
+          <button>Resume</button>
+        </li>
+        <li>
+          <button>Recipes</button>
+        </li>
+        <li>
+          <button>Contact</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default sidebar;
---
+++ src/Containers/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../Components/Sidebar/Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cassie Brown</h1>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
